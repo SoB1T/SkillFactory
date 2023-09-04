7fbd29b093a96278aec50c0b15c777110e143a79
SoB <sob.work.it@gmail.com>
2023-09-05 01:53:03 +0300

    Update Test_cps.py

--- Test_cps.py
+++ Test_cps.py
@@ -1,11 +1,52 @@
 import random
 
 
+
 class Ship:
-    def __init__(self, size):
+    def __init__(self, size, x, y, rotation):
+        self.x = x
+        self.y = y
         self.size = size
         self.hp = size
-        self.coords = set()
+        self.rotation = rotation
+        self.coords = self._get_coords(x, y, size, rotation)
+        self.aura = self._get_coords_aura(x, y, size, rotation, self.coords)
+
+    def _get_coords_aura(self, x, y, size, rotation, coords):
+        aura = set()
+        if rotation == "left" or rotation == "right":
+            for d in range(y - 1, y + 2):
+                for i in range(x - 1, x + size + 1):
+                    if (i, d) in coords:
+                        continue
+                    else:
+                        aura.add((i, d))
+            return tuple(aura)
+        if rotation == "up" or rotation == "dawn":
+            for d in range(y - 1, y + size + 1):
+                for i in range(x - 1, x + 2):
+                    if (i, d) in coords:
+                        continue
+                    else:
+                        aura.add((i, d))
+            return tuple(aura)
+
+    def _get_coords(self, x, y, size, rotation):
+        coords = set()
+        if rotation == "up":
+            for i in range(size):
+                coords.add((x, y - i))
+        if rotation == "dawn":
+            for i in range(size):
+                coords.add((x, y + i))
+        if rotation == "right":
+            for i in range(size):
+                coords.add((x - i, y))
+        if rotation == "left":
+            for i in range(size):
+                coords.add((x + i, y))
+
+        return tuple(coords)
 
     def hit(self):
         self.hp -= 1
@@ -13,21 +54,35 @@ class Ship:
     def destrou(self):
         return self.hp == 0
 
+
 class Board:
     def __init__(self, size):
         self.size = size
         self.grid = [["~" for i in range(size)] for i in range(size)]
         self.radar = [["~" for i in range(size)] for i in range(size)]
-        self.ships_cords = []
+        self.ships_cords = set()
         self.ships = []
+        self.size_ship_vars = []  # костыль что бы во внешней логике было достаточно вызвать Board и предоставить size_ship_vars
+        self.board_coords = self._get_coords_board(size)
 
-    def ship_randomizer(self, ship):
+    def _get_coords_board(self, size):
+        coords = set()
+        for i in range(0, size + 1):
+            for d in range(0, size + 1):
+                coords.add((i, d))
+        return tuple(coords)
+
+    def ship_randomizer(self, ship_vars):
         while True:
+            size = ship_vars
             x = random.randint(0, self.size - 1)
             y = random.randint(0, self.size - 1)
             rotation = random.choice(["left", "right", "up", "dawn"])
-            if self.can_plays_ship(ship, rotation, x, y):
-                if self.other_ship(ship, rotation, x, y):
+            Ship(int(size), x, y, rotation)
+            ship = Ship._get_coords
+            aura = Ship._get_coords_aura
+            if self.can_plays_ship(ship):
+                if self.other_ship(aura, self.ships_cords):
                     self.add_ship(ship, rotation, x, y)
                     break
                 else:
@@ -35,53 +90,18 @@ class Board:
             else:
                 continue
 
-    def can_plays_ship(self, ship, rotation, x, y):
-        if rotation == "left":
-            if x == 5 and ship.size > 1:
-                return False
-            else:
-                return True
-        elif rotation == "right":
-            if x == 0 and ship.size > 1:
-                return False
-            else:
-                return True
-        elif rotation == "up":
-            if y == 5 and ship.size > 1:
-                return False
-            else:
-                return True
-        elif rotation == "dawn":
-            if y == 0 and ship.size > 1:
-                return False
-            else:
-                return True
+    def can_plays_ship(self, ship):
+        if ship in self.board_coords:
+            return True
+        else:
+            return False
 
-    def other_ship(self, ship, rotation, x, y):
-        if rotation == "left" or rotation == "right":
-            for d in range(y - 1, y + 2):
-                for i in range(x - 1, x + ship.size + 1):
-                    if d == y and i == x:
-                        continue
-                    elif d == y and i == x + ship.size:
-                        continue
-                    else:
-                        if [i, d] in self.ships_cords:
-                            return False
-                        else:
-                            return True
-        if rotation == "up" or rotation == "dawn":
-            for d in range(y - 1, y + ship.size + 1):
-                for i in range(x - 1, x + 2):
-                    if d == y and i == x:
-                        continue
-                    elif d == y + ship.size and i == x:
-                        continue
-                    else:
-                        if [i, d] in self.ships_cords:
-                            return False
-                        else:
-                            return True
+    def other_ship(self, aura, ships_cords):
+        intersec = ships_cords.intersection(aura)
+        if intersec:
+            return False
+        else:
+            return True
 
     def add_ship(self, ship, rotation, x, y):
 
@@ -90,22 +110,21 @@ class Board:
             for i in range(ship.size):
                 self.grid[x + i][y] = "■"
                 self.ships_cords.append([x + i, y])
-                ship.coords.add((x + i, y))
+
         elif rotation == "right":
             for i in range(ship.size):
                 self.grid[x - i][y] = "■"
                 self.ships_cords.append([x - i, y])
-                ship.coords.add((x - i, y))
+
         elif rotation == "up":
             for i in range(ship.size):
                 self.grid[x][y + i] = "■"
                 self.ships_cords.append([x, y + i])
-                ship.coords.add((x, y + i))
+
         elif rotation == "dawn":
             for i in range(ship.size):
                 self.grid[x][y - i] = "■"
                 self.ships_cords.append([x, y - i])
-                ship.coords.add((x, y - i))
 
     # self.Ship.nose
 
@@ -128,14 +147,14 @@ def main():
     player_board = Board(6)
     computer_board = Board(6)
 
-    player_ships = [Ship(3), Ship(2), Ship(2), Ship(1), Ship(1), Ship(1), Ship(1)]
-    computer_ships = [Ship(3), Ship(2), Ship(2), Ship(1), Ship(1), Ship(1), Ship(1)]
+    size_ship_vars = ["1", "1", "1", "1", "2", "2", "3"]
+    player_ships = size_ship_vars
+    comp_ships = size_ship_vars
+    for ship_vars in player_ships:
+        player_board.ship_randomizer(ship_vars)
 
-    for ship in player_ships:
-        player_board.ship_randomizer(ship)
-
-    for ship in computer_ships:
-        computer_board.ship_randomizer(ship)
+    for ship_vars in comp_ships:
+        computer_board.ship_randomizer(ship_vars)
 
     player_moves = set()
     computer_moves = set()
@@ -157,14 +176,14 @@ def main():
 
             if computer_board.grid[y][x] == '■':
                 print("Вы подбили вражеский корабль!")
-                for ship in computer_ships:
+                for ship in comp_ships:
                     if (y, x) in ship.coords:
                         ship.hit()
                         if ship.destrou():
                             print("Вражеский корабль потоплен!")
                             for coord in ship.coords:
                                 computer_board.grid[coord[0]][coord[1]] = 'X'
-                            computer_ships.remove(ship)
+                            comp_ships.remove(ship)
                         else:
                             computer_board.grid[y][x] = 'X'
             else:
@@ -193,10 +212,10 @@ def main():
                 print("Компьютер промахнулся!")
                 player_board.grid[computer_move[0]][computer_move[1]] = 'T'
 
-            for ship in computer_ships:
+            for ship in comp_ships:
                 print(f"Компьютер: Корабль размером {ship.size}, попаданий: {ship.hit}, координаты: {ship.coords}")
 
-            if all(ship.destrou() for ship in computer_ships):
+            if all(ship.destrou() for ship in comp_ships):
                 print("Поздравляю! Вы выиграли!")
                 break
 
@@ -206,7 +225,7 @@ def main():
 
             print("Состояние игры:")
             print("Корабли противника:")
-            for ship in computer_ships:
+            for ship in comp_ships:
                 print(f"Корабль размером {ship.size}, потоплен: {ship.destrou()}")
             print("Ваши корабли:")
             for ship in player_ships:
